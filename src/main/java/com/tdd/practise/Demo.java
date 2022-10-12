777ea3a913b350ccd4f236198bedc082381d03d4
IGoodBoy6 <1599081381@qq.com>
2022-10-12 13:19:32 +0800

    第二次提交整个项目

---
+++ src/main/java/com/tdd/practise/Demo.java
@@ -0,0 +1,17 @@
+package com.tdd.practise;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author gongchengqiang
+ * @Create 2022-10-12-13:14
+ */
+public class Demo {
+
+    private static final Logger logger = LoggerFactory.getLogger(Demo.class);
+
+    public static void main(String[] args) {
+        logger.info("哒哒哒");
+    }
+}
